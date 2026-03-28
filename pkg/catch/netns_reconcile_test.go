cb8df662c79fb9c238308be3806815027557ea36
shayne <79330+shayne@users.noreply.github.com>
2026-03-28 13:40:21 -0400

    pkg/catch: reconcile stale compose netns on startup

---
+++ pkg/catch/netns_reconcile_test.go
@@ -0,0 +1,123 @@
+// Copyright (c) 2025 AUTHORS All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package catch
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/yeetrun/yeet/pkg/db"
+)
+
+type fakeDockerNetNSReconciler struct {
+	name      string
+	reconcile func() (bool, error)
+}
+
+func (f fakeDockerNetNSReconciler) ReconcileNetNS() (bool, error) {
+	return f.reconcile()
+}
+
+func TestReconcileNetNSBackedDockerServices(t *testing.T) {
+	s := newTestServer(t)
+	svcs := []db.Service{
+		{
+			Name:             "docker-netns",
+			ServiceType:      db.ServiceTypeDockerCompose,
+			Generation:       1,
+			LatestGeneration: 1,
+			Artifacts: db.ArtifactStore{
+				db.ArtifactNetNSService: {Refs: map[db.ArtifactRef]string{db.Gen(1): "/tmp/yeet-docker-netns-ns.service"}},
+			},
+		},
+		{
+			Name:             "docker-plain",
+			ServiceType:      db.ServiceTypeDockerCompose,
+			Generation:       1,
+			LatestGeneration: 1,
+		},
+		{
+			Name:             "systemd-netns",
+			ServiceType:      db.ServiceTypeSystemd,
+			Generation:       1,
+			LatestGeneration: 1,
+			Artifacts: db.ArtifactStore{
+				db.ArtifactNetNSService: {Refs: map[db.ArtifactRef]string{db.Gen(1): "/tmp/yeet-systemd-netns-ns.service"}},
+			},
+		},
+	}
+	for _, svc := range svcs {
+		svc := svc
+		if _, _, err := s.cfg.DB.MutateService(svc.Name, func(_ *db.Data, stored *db.Service) error {
+			*stored = svc
+			return nil
+		}); err != nil {
+			t.Fatalf("MutateService(%q): %v", svc.Name, err)
+		}
+	}
+
+	var called []string
+	s.newDockerComposeService = func(name string) (dockerNetNSReconciler, error) {
+		return fakeDockerNetNSReconciler{
+			name: name,
+			reconcile: func() (bool, error) {
+				called = append(called, name)
+				return name == "docker-netns", nil
+			},
+		}, nil
+	}
+
+	if err := s.reconcileNetNSBackedDockerServices(); err != nil {
+		t.Fatalf("reconcileNetNSBackedDockerServices returned error: %v", err)
+	}
+	if diff := cmp.Diff([]string{"docker-netns"}, called); diff != "" {
+		t.Fatalf("unexpected reconciled services (-want +got):\n%s", diff)
+	}
+}
+
+func TestServerStartRunsNetNSReconciliation(t *testing.T) {
+	s := newTestServer(t)
+	if _, _, err := s.cfg.DB.MutateService("docker-netns", func(_ *db.Data, stored *db.Service) error {
+		*stored = db.Service{
+			Name:             "docker-netns",
+			ServiceType:      db.ServiceTypeDockerCompose,
+			Generation:       1,
+			LatestGeneration: 1,
+			Artifacts: db.ArtifactStore{
+				db.ArtifactNetNSService: {Refs: map[db.ArtifactRef]string{db.Gen(1): "/tmp/yeet-docker-netns-ns.service"}},
+			},
+		}
+		return nil
+	}); err != nil {
+		t.Fatalf("MutateService(docker-netns): %v", err)
+	}
+
+	var calls []string
+	prevInstall := installYeetNSService
+	installYeetNSService = func() error {
+		calls = append(calls, "install")
+		return nil
+	}
+	defer func() {
+		installYeetNSService = prevInstall
+	}()
+
+	s.newDockerComposeService = func(name string) (dockerNetNSReconciler, error) {
+		return fakeDockerNetNSReconciler{
+			name: name,
+			reconcile: func() (bool, error) {
+				calls = append(calls, "reconcile:"+name)
+				return false, nil
+			},
+		}, nil
+	}
+
+	s.Start()
+	t.Cleanup(s.Shutdown)
+
+	if diff := cmp.Diff([]string{"install", "reconcile:docker-netns"}, calls); diff != "" {
+		t.Fatalf("unexpected startup call order (-want +got):\n%s", diff)
+	}
+}
