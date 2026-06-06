118ab8befc856d10ade01527c3c676dcebf8285b
shayne <79330+shayne@users.noreply.github.com>
2026-06-06 17:37:13 -0400

    vm: adopt Ubuntu-compatible v12 image

--- pkg/catch/vm_image.go
+++ pkg/catch/vm_image.go
@@ -23,7 +23,7 @@ import (
 )
 
 const (
-	defaultVMImageVersion     = "ubuntu-26.04-amd64-v11"
+	defaultVMImageVersion     = "ubuntu-26.04-amd64-v12"
 	defaultVMImageManifestURL = "https://github.com/yeetrun/yeet-vm-images/releases/latest/download/manifest.json"
 
 	vmImageCacheMissing = "missing"
