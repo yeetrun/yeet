e79b27b2f75e9d0a889a804b41e72d9349d91740
shayne <79330+shayne@users.noreply.github.com>
2026-06-07 21:35:52 -0400

    catch: fix NixOS VM SSH metadata permissions

--- pkg/catch/vm_metadata_test.go
+++ pkg/catch/vm_metadata_test.go
@@ -293,6 +293,7 @@ func TestWriteVMGuestMetadataFilesUsesNixOSDriver(t *testing.T) {
 	assertFileContains(t, filepath.Join(root, "etc", "yeet-vm", "hostname"), "nixos-smoke")
 	assertFileContains(t, filepath.Join(root, "etc", "yeet-vm", "user"), "nixos")
 	assertFileContains(t, filepath.Join(root, "etc", "yeet-vm", "authorized_keys"), "ssh-ed25519 AAAATEST")
+	assertFileMode(t, filepath.Join(root, "etc", "yeet-vm", "authorized_keys"), 0o644)
 	assertFileContains(t, filepath.Join(root, "etc", "yeet-vm", "systemd-network", "10-yeet-eth0.network"), "Address=192.168.100.12/24")
 
 	for _, path := range []string{
